e7ffc20df540206be9f48b6cc7e0598b5e35591a
Samme Qandil <saqandil@gmail.com>
2018-07-25 23:30:15 -0700

    new classes with fragments
    blah

---
+++ app/src/main/java/io/github/anoobishnoob/recipeapp/ListFragment.java
@@ -0,0 +1,23 @@
+package io.github.anoobishnoob.recipeapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by saqan on 7/25/2018.
+ */
+
+public class ListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_list, container);
+        return view;
+    }
+}
