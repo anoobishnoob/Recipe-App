c6fa9d9581ed01c197f13320ccf64b33e8c55f42
Samme Qandil <saqandil@gmail.com>
2018-07-31 03:57:37 -0700

    some more changes
    still not a functioning app

--- app/src/main/java/io/github/anoobishnoob/recipeapp/ListFragment.java
+++ app/src/main/java/io/github/anoobishnoob/recipeapp/ListFragment.java
@@ -17,7 +17,7 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.fragment_list, container);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
         return view;
     }
 }
