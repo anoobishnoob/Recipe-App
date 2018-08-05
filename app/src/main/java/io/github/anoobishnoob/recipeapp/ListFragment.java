515955c2bd80c916e5e649cb02792388d3fa3378
Samme Qandil <saqandil@gmail.com>
2018-08-05 04:51:40 -0700

    Implemted the List adapter and RecyclerVIew
    I have now created the standard UI for the app

--- app/src/main/java/io/github/anoobishnoob/recipeapp/ListFragment.java
+++ app/src/main/java/io/github/anoobishnoob/recipeapp/ListFragment.java
@@ -3,6 +3,8 @@ package io.github.anoobishnoob.recipeapp;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,8 +18,14 @@ public class ListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        //return super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.listRecyclerView);
+        ListAdapter listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
         return view;
     }
 }
