397346d1e72c5c9e07e999f3b7b3979c2dcd7567
Samme Qandil <saqandil@gmail.com>
2018-08-09 03:55:11 -0700

    Refactored my app

--- app/src/main/java/io/github/anoobishnoob/recipeapp/ViewPagerFragment.java
+++ app/src/main/java/io/github/anoobishnoob/recipeapp/ViewPagerFragment.java
@@ -4,14 +4,13 @@ package io.github.anoobishnoob.recipeapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.CheckBox;
 
 /**
  * Created by saqan on 8/7/2018.
